6cfd79f1a5ffc515307f160e48738547115a7876
Hridoy <rakibhasanhridoy776@gmail.com>
2023-01-31 18:56:13 +0600

    check-out page partially done

---
+++ src/components/check-out/OrderProceedTable.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+type Props = {
+    className:string;
+};
+
+const OrderProceedTable = (props: Props) => {
+  return (
+    <div className=" w-fit h-fit my-[60px] mx-auto bg-[#151515] px-[40px] py-[30px]">
+      <h1 className="text-2xl uppercase mb-8">your order</h1>
+
+      <table className=" shop_table  ">
+        <thead>
+          <tr className=" ">
+            <th className=" ">order</th>
+            <th className=" ">Contact</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="">
+            <th className=" ">order</th>
+            <th className=" ">Contact</th>
+          </tr>
+          <tr className="">
+            <th className=" ">order</th>
+            <th className=" ">Contact</th>
+          </tr>
+          <tr className="">
+            <th className=" ">order</th>
+            <th className=" ">Contact</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default OrderProceedTable;
---
+++ src/pages/check-out/index.tsx
@@ -0,0 +1,26 @@
+import BealingDetails from "@/components/check-out/BealingDetails";
+import OrderProceedTable from "@/components/check-out/OrderProceedTable";
+import Cartbanner from "@/components/common/Cartbanner";
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/navbar";
+import React from "react";
+
+type Props = {};
+
+const CheckOut = (props: Props) => {
+  return (
+    <div className="bg-black text-white">
+      <Navbar />
+      <section className=" ">
+        <Cartbanner />
+        <div className="w-[1200px] mx-auto grid grid-cols-2 gap-4 justify-center ">
+          <BealingDetails />
+          <OrderProceedTable className="mx-auto" />
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+};
+
+export default CheckOut;
